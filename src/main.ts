3028bdc4ed34a91a6c00d7d78dfc5cd4e179e694
sunhaoxiang <673686754@qq.com>
2025-02-05 16:35:38 +0800

    chore: Add project dependencies and UnoCSS configuration

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
+import 'virtual:uno.css'
+// import '@unocss/reset/tailwind.css'
+// import '@unocss/reset/tailwind-compat.css'
+
 import './style.css'
 import App from './App.vue'
 
