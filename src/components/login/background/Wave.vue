6d0c785957d35824e802935cb9e11bdcbdcb4299
sunhaoxiang <673686754@qq.com>
2025-02-06 23:58:05 +0800

    refactor: Migrate wave background styles to UnoCSS and remove module stylesheet

--- src/components/login/background/Wave.vue
+++ src/components/login/background/Wave.vue
@@ -1,10 +1,6 @@
-<script setup lang="ts">
-import styles from './Wave.module.scss'
-</script>
-
 <template>
   <svg
-    :class="styles.waves"
+    class="login-bg-waves"
     xmlns="http://www.w3.org/2000/svg"
     xmlnsXlink="http://www.w3.org/1999/xlink"
     viewBox="0 24 150 28"
@@ -17,11 +13,11 @@ import styles from './Wave.module.scss'
         d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z"
       />
     </defs>
-    <g :class="styles.parallax">
-      <use href="#gentle-wave" x="48" y="0" fill="rgba(255,255,255,0.7)" />
-      <use href="#gentle-wave" x="48" y="3" fill="rgba(255,255,255,0.5)" />
-      <use href="#gentle-wave" x="48" y="5" fill="rgba(255,255,255,0.3)" />
-      <use href="#gentle-wave" x="48" y="7" fill="#fff" />
+    <g>
+      <use class="login-bg-wave-1" href="#gentle-wave" x="48" y="0" fill="rgba(255,255,255,0.7)" />
+      <use class="login-bg-wave-2" href="#gentle-wave" x="48" y="3" fill="rgba(255,255,255,0.5)" />
+      <use class="login-bg-wave-3" href="#gentle-wave" x="48" y="5" fill="rgba(255,255,255,0.3)" />
+      <use class="login-bg-wave-4" href="#gentle-wave" x="48" y="7" fill="#fff" />
     </g>
   </svg>
 </template>
