694fe66b399659313a5fff4654df0edc2937807f
sunhaoxiang <673686754@qq.com>
2025-02-07 09:42:57 +0800

    refactor: Rename UnoCSS animation keyframes for login wave background

--- uno.config.ts
+++ uno.config.ts
@@ -2,32 +2,32 @@ import { defineConfig, presetUno } from 'unocss'
 
 export default defineConfig({
   rules: [
-    // login background
+    // background login
     ['bg-login', { background: 'linear-gradient(60deg, rgba(84, 58, 183, 1) 0%, rgba(0, 172, 193, 1) 100%)' }],
   ],
   shortcuts: {
     'icon-wrapper': 'inline-block leading-0 h-1em w-1em text-inherit normal-case text-center align-[-0.125em] antialiased [&_svg]:inline-block [&_svg]:align-baseline',
 
-    // login background waves
+    // background login waves
     'bg-login-waves': 'relative w-full h-20vh mb-[-7px] min-h-100px',
-    'bg-login-wave-1': 'animate-move-forever animate-delay-[-2s] animate-duration-[7s]',
-    'bg-login-wave-2': 'animate-move-forever animate-delay-[-3s] animate-duration-[10s]',
-    'bg-login-wave-3': 'animate-move-forever animate-delay-[-4s] animate-duration-[13s]',
-    'bg-login-wave-4': 'animate-move-forever animate-delay-[-5s] animate-duration-[20s]',
+    'bg-login-wave-1': 'animate-wave-move animate-delay-[-2s] animate-duration-[7s]',
+    'bg-login-wave-2': 'animate-wave-move animate-delay-[-3s] animate-duration-[10s]',
+    'bg-login-wave-3': 'animate-wave-move animate-delay-[-4s] animate-duration-[13s]',
+    'bg-login-wave-4': 'animate-wave-move animate-delay-[-5s] animate-duration-[20s]',
   },
   theme: {
     animation: {
       keyframes: {
-        'move-forever': '{ 0% {transform: translate3d(-90px, 0, 0); } 100% {transform: translate3d(85px, 0, 0); }}',
+        'wave-move': '{ 0% {transform: translate3d(-90px, 0, 0); } 100% {transform: translate3d(85px, 0, 0); }}',
       },
       durations: {
-        'move-forever': '25s',
+        'wave-move': '25s',
       },
       timingFns: {
-        'move-forever': 'cubic-bezier(0.55, 0.5, 0.45, 0.5)',
+        'wave-move': 'cubic-bezier(0.55, 0.5, 0.45, 0.5)',
       },
       counts: {
-        'move-forever': 'infinite',
+        'wave-move': 'infinite',
       },
     },
   },
