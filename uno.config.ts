aa5c2458cecce23181899a468d03a30002d5246e
sunhaoxiang <673686754@qq.com>
2025-02-07 23:31:37 +0800

    feat: Add custom vertical alignment rule to UnoCSS configuration

--- uno.config.ts
+++ uno.config.ts
@@ -4,10 +4,10 @@ export default defineConfig({
   rules: [
     // background login
     ['bg-login', { background: 'linear-gradient(60deg, rgba(84, 58, 183, 1) 0%, rgba(0, 172, 193, 1) 100%)' }],
+    [/^align-\[([+-]?\d.+)\]$/, ([_, num]) => ({ 'vertical-align': num })],
   ],
   shortcuts: {
     'icon-wrapper': 'inline-block leading-0 h-1em w-1em text-inherit normal-case text-center align-[-0.125em] antialiased [&_svg]:inline-block [&_svg]:align-baseline',
-
     // background login waves
     'bg-login-waves': 'relative w-full h-20vh mb-[-7px] min-h-100px',
     'bg-login-wave-1': 'animate-wave-move animate-delay-[-2s] animate-duration-[7s]',
