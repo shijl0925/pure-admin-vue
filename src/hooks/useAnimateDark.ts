72818a97c8ac3a8471576df29e6015df7e0bc720
sunhaoxiang <673686754@qq.com>
2025-02-10 19:40:21 +0800

    fix: Adjust view transition z-index styling for dark mode

--- src/hooks/useAnimateDark.ts
+++ src/hooks/useAnimateDark.ts
@@ -24,7 +24,6 @@ function injectBaseStyles() {
         .dark::view-transition-new(root) {
           z-index: 1;
         }
-
         ::view-transition-new(root),
         .dark::view-transition-old(root) {
           z-index: 9999;
