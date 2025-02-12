f5a94ac8978467bb2d83e805e67fbae9c2195f90
sunhaoxiang <673686754@qq.com>
2025-02-12 20:18:03 +0800

    feat: Add dynamic Ant Design locale support based on selected language

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,31 @@
 <script setup lang="ts">
+import enUS from 'ant-design-vue/es/locale/en_US'
 import zhCN from 'ant-design-vue/es/locale/zh_CN'
 import { storeToRefs } from 'pinia'
+import { computed } from 'vue'
+import { useI18n } from 'vue-i18n'
 import { RouterView } from 'vue-router'
 
 import { useAppStore } from '@/stores/app'
 
 const appStore = useAppStore()
 const { antdTheme } = storeToRefs(appStore)
+const { locale } = useI18n()
+
+const antdLocale = computed(() => {
+  switch (locale.value) {
+    case 'en-US':
+      return enUS
+    case 'zh-CN':
+      return zhCN
+    default:
+      return zhCN
+  }
+})
 </script>
 
 <template>
-  <a-config-provider :locale="zhCN" :theme="antdTheme">
+  <a-config-provider :locale="antdLocale" :theme="antdTheme">
     <a-app>
       <RouterView />
     </a-app>
