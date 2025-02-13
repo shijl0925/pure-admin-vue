4b7d33529f97317d64c37e7845002abbfa9abe52
sunhaoxiang <673686754@qq.com>
2025-02-13 08:50:09 +0800

    feat: Improve icon and text rendering styles in UnoCSS configuration

--- uno.config.ts
+++ uno.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
     // background login
     ['bg-login', { background: 'linear-gradient(60deg, rgba(84, 58, 183, 1) 0%, rgba(0, 172, 193, 1) 100%)' }],
     [/^align-\[([+-]?\d.+)\]$/, ([_, num]) => ({ 'vertical-align': num })],
+    ['text-optimizeLegibility', { 'text-rendering': 'optimizeLegibility' }],
   ],
   shortcuts: {
     // theme
@@ -15,7 +16,7 @@ export default defineConfig({
     'bg-theme-content': 'bg-light-300',
     'bg-theme-content-dark': 'bg-black',
     // icon
-    'icon-wrapper': 'inline-block leading-0 h-1em w-1em text-inherit normal-case text-center align-[-0.125em] antialiased [&_svg]:inline-block [&_svg]:align-baseline',
+    'icon-wrapper': 'inline-block leading-0 h-1em w-1em text-inherit font-not-italic normal-case text-center align-[-0.125em] text-optimizeLegibility antialiased [&_svg]:inline-block [&_svg]:align-baseline',
     // background login waves
     'bg-login-waves': 'relative w-full h-20vh mb-[-7px] min-h-100px',
     'bg-login-wave-1': 'animate-wave-move animate-delay-[-2s] animate-duration-[7s]',
