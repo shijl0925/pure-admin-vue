6edf3d5bb160f4a66e7372a103152b141bc23674
sunhaoxiang <673686754@qq.com>
2025-02-16 12:42:24 +0800

    fix: Update login API endpoint to correct route

--- src/apis/user.ts
+++ src/apis/user.ts
@@ -3,7 +3,7 @@ import type { AuthTokens, LoginData, UserInfo } from '@/types/user'
 import { http } from '@/utils/http'
 
 export function loginApi(data: LoginData): Promise<AuthTokens> {
-  return http.post('/auth/login', data)
+  return http.post('/user/login', data)
 }
 
 export function userInfoApi(): Promise<UserInfo> {
