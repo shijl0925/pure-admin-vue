4ecaa62feab5f70c3cba0426625bd4fac62c32e1
sunhaoxiang <673686754@qq.com>
2025-02-19 19:08:41 +0800

    refactor: Fine-tune SearchContainer motion transition duration

--- src/components/container/SearchContainer.vue
+++ src/components/container/SearchContainer.vue
@@ -18,7 +18,7 @@ const isVisible = ref(false)
       :initial="{ height: 0, opacity: 0 }"
       :animate="{ height: 'auto', opacity: 1 }"
       :exit="{ height: 0, opacity: 0 }"
-      :transition="{ duration: 0.2, ease: 'easeInOut' }"
+      :transition="{ duration: 0.25, ease: 'easeInOut' }"
     >
       <slot name="extra" />
     </Motion>
