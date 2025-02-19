b6fb07f4d59d09d1447a763de686db446a25fb27
sunhaoxiang <673686754@qq.com>
2025-02-19 19:08:13 +0800

    feat: Add page transition animation to admin base layout

--- src/pages/(admin)/(base-layout).vue
+++ src/pages/(admin)/(base-layout).vue
@@ -4,6 +4,16 @@ import { BaseLayout } from '@/layouts'
 
 <template>
   <BaseLayout>
-    <RouterView />
+    <RouterView v-slot="{ Component }">
+      <Transition
+        enter-active-class="transition-all duration-300"
+        leave-active-class="transition-all duration-300"
+        enter-from-class="opacity-0 -translate-x-4"
+        leave-to-class="opacity-0 translate-x-4"
+        mode="out-in"
+      >
+        <component :is="Component" />
+      </Transition>
+    </RouterView>
   </BaseLayout>
 </template>
