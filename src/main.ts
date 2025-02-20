59fe412739b801bbf08fc3c254e9ebecf1bd87c1
sunhaoxiang <673686754@qq.com>
2025-02-20 17:18:27 +0800

    feat: Add Vue Query for state management and data fetching

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import iconParkOutline from '@iconify-json/icon-park-outline/icons.json'
 import { addCollection } from '@iconify/vue'
+import { VueQueryPlugin } from '@tanstack/vue-query'
 import Antd from 'ant-design-vue'
 import { createPinia } from 'pinia'
-import { createApp } from 'vue'
 import 'ant-design-vue/dist/reset.css'
 import 'virtual:uno.css'
 import 'virtual:local-icons'
+import { createApp } from 'vue'
 
 import { i18n } from '@/locales'
 
@@ -20,4 +21,5 @@ createApp(App)
   .use(router)
   .use(Antd)
   .use(i18n)
+  .use(VueQueryPlugin)
   .mount('#app')
