a66a475ef9169a5e40be35436d73e40042f33336
sunhaoxiang <673686754@qq.com>
2025-03-18 10:02:58 +0800

    feat: 添加 vite-plugin-compression 插件以支持构建时文件压缩，并更新 Vite 配置以启用该功能

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { fileURLToPath, URL } from 'node:url'
 import UnoCSS from 'unocss/vite'
 import VueRouter from 'unplugin-vue-router/vite'
 import { defineConfig, loadEnv } from 'vite'
+import viteCompression from 'vite-plugin-compression'
 
 import { setPermissionMeta } from './src/router/routeMeta'
 import { svgToIconify } from './vite-plugins'
@@ -18,6 +19,21 @@ export default defineConfig(({ mode }) => {
     base: VITE_BASE_URL,
     server: {
       port: Number.parseInt(VITE_PORT),
+    },
+    build: {
+      chunkSizeWarningLimit: 1000,
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            'vue': ['vue', 'vue-router', 'pinia'],
+            'ant-design-vue': ['ant-design-vue'],
+            'echarts': ['echarts'],
+          },
+          chunkFileNames: 'assets/js/[name]-[hash].js',
+          entryFileNames: 'assets/js/[name]-[hash].js',
+          assetFileNames: 'assets/[ext]/[name]-[hash].[ext]',
+        },
+      },
     },
     plugins: [
       VueRouter({
@@ -37,6 +53,14 @@ export default defineConfig(({ mode }) => {
       VueI18nPlugin({
         include: resolve(dirname(fileURLToPath(import.meta.url)), './src/locales/langs/**'),
         // include: resolve(__dirname, './src/locales/langs/**'),
+      }),
+      viteCompression({
+        verbose: true, // 是否在控制台输出压缩结果
+        disable: false, // 是否禁用
+        threshold: 1024, // 体积大于阈值会被压缩，单位为 b，默认为 1kb
+        algorithm: 'gzip', // 压缩算法，可选 ['gzip','brotliCompress','deflate','deflateRaw']
+        ext: '.gz', // 生成的压缩包后缀
+        deleteOriginFile: false, // 是否删除原文件，默认为 false
       }),
     ],
     resolve: {
